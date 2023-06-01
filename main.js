d2d80098230f928ee754165797655e934bf138e1
ReubenMay <95076512+ReubenMay@users.noreply.github.com>
2023-06-01 07:02:57 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 function setup(){
+    video=createCapture(VIDEO)
+    video.size(550,500)
+    
     canvas=createCanvas(500,500);
-    canvas.position(450,100);
+    canvas.position(650,100);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses)
+    
+}
+function modelLoaded(){
+    console.log("poseNet Is initalized")
+}
+function draw(){
+    background("#8179f2")
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results)
+    }
 }
